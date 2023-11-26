dd67d32916e97b68140d8187a98a86167105548e
pandablue0809 <bluep0415@gmail.com>
2023-11-26 20:23:55 +0530

    style(login): style sign in/up container ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./Home.css";
-import React, { useContext, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Login from "@/components/Login/Login";
 import SignUp from "@/components/SignUp/SignUp";
@@ -9,11 +9,10 @@ import Flex from "./styles/Flex.styled";
 import Text from "./styles/Text.styled";
 import Container from "./styles/Container.styled";
 import Theme from "./styles/Theme.styled";
-import { DarkLightModeContext } from "./contexts/DarkLightModeProvider";
+import Link from "next/link";
 
 const Home = () => {
   const router = useRouter();
-  const { isDark } = useContext(DarkLightModeContext)!;
 
   useEffect(() => {
     const userInfoString = localStorage.getItem("userInfo");
@@ -34,37 +33,37 @@ const Home = () => {
         justifyContent="flex-end"
         mJustifyContent="flex-start"
       >
-        <Text
-          className="hero-img-logo"
-          fontSize="2rem"
-          mFontSize="1rem"
-          fontWeight="900"
-          letterSpacing="16px"
-          color={Theme.colors.white}
-        >
-          BYTEPING
-        </Text>
-        <Container border="2px solid white" padding="48px" mPadding="24px">
-          <Text fontSize="3.5rem" fontWeight="100" color={Theme.colors.white} letterSpacing="4px">
+        <Link href={"/"} className="hero-img-logo">
+          <Text
+            className="text-with-border"
+            fontSize="2rem"
+            mFontSize="1rem"
+            fontWeight="900"
+            letterSpacing="16px"
+            color={Theme.colors.white}
+          >
+            BYTEPING
+          </Text>
+        </Link>
+        <Container border="2px solid white" padding="48px" mPadding="24px" width="528px" mWidth="324px">
+          <Text fontSize="3.5rem" mFontSize="32px" fontWeight="100" color={Theme.colors.white} letterSpacing="4px">
             IMPRESSIVE EXPERIENCES THAT DELIVER
           </Text>
         </Container>
       </Flex>
-      <Flex backgroundColor="#FBBC05" height="100vh">
-        Features
-      </Flex>
       <Flex
-        backgroundColor={isDark ? Theme.colors.black : Theme.colors.white}
+        backgroundColor={Theme.colors.white}
         height="100vh"
         padding="48px"
         mPadding="24px"
         justifyContent="center"
         alignItems="center"
       >
-        <Flex mFlexDirection="column" gap="32px">
+        <Flex mFlexDirection="column" width="50%">
           <Login />
           <SignUp />
         </Flex>
+        <Flex className="sign-in-up-preview" $mDisplay="none" width="45%" height="75%" margin="0 24px 0 0"></Flex>
       </Flex>
     </Container>
   );
